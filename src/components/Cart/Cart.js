695e8d73c77491d01f3c36193c8f5cc3811d8e73
Abhishek Shukla <abhishek.shukla@xoriant.com>
2019-01-28 12:36:03 +0530

    First Commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Title from "../Title";
+import { ProductConsumer } from "../../Context";
+import Product from "../Product";
+import CartColumns from "./CartColumns";
+import EmptyCart from "./EmptyCart";
+import CartList from "./CartList";
+import CartTotals from './CartTotals';
+
+export default class Cart extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <ProductConsumer>
+          {value => {
+            const { cart } = value;
+            if (cart.length > 0) {
+              return (
+                <React.Fragment>
+                  <Title name="Your" title="Cart" />
+                  <CartColumns />
+                  <CartList value={value} />
+                  <CartTotals value={value} />
+                  
+                </React.Fragment>
+              );
+            } else {
+              return <EmptyCart />;
+            }
+          }}
+        </ProductConsumer>
+      </div>
+    );
+  }
+}
